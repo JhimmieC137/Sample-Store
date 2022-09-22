e4a5c163068e7fb6dc89aba1fdbc693fecdb9ea3
JhimmieC137 <toluwalope.david@gmail.com>
2022-09-22 10:27:46 +0100

    wrapping up

--- userprofile/tests.py
+++ userprofile/tests.py
@@ -1,3 +1,8 @@
+from urllib import response
 from django.test import TestCase
 
 # Create your tests here.
+class URLTests(TestCase):
+    def test_testregistration(self):
+        response = self.client.get('register/')
+        self.assertEqual(response.status_code, 200)
